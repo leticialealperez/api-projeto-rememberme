e7d307139f2a8e35927c1c83a1da42d0e36eb5c8
Leticia Leal <leticialealp@hotmail.com>
2021-12-05 23:20:40 -0300

    fixed cors issue

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import express, {NextFunction, Request, Response} from 'express'
 import 'dotenv/config'
-
+import cors from 'cors';
 
 const app = express();
 
 const port = process.env.PORT
 
 app.use(express.json());
-
+app.use(cors());
 
 app.listen(port, ()=>{
     console.log(`>>> Started server on PORT ${port}`);
